f90b6908b799fa661a6bf06b41299a1ca14546de
Brendan Lewis <brendanelewis@outlook.com>
2020-10-02 08:52:48 -0400

    Compare block by class

--- src/main/java/dev/sebastianb/cringemod/mixin/OnUse.java
+++ src/main/java/dev/sebastianb/cringemod/mixin/OnUse.java
@@ -3,6 +3,7 @@ package dev.sebastianb.cringemod.mixin;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.DragonEggBlock;
+import net.minecraft.block.RespawnAnchorBlock;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
@@ -15,34 +16,21 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+
 /*
 Wait it's all mixins!
 Always has been...
  */
-
 @Mixin(DragonEggBlock.class)
 public abstract class OnUse {
 
-    @Inject(method = "onUse",
-            at = @At("HEAD"), cancellable = true)
+    @Inject(method = "onUse", at = @At("HEAD"), cancellable = true)
     private void used(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit, CallbackInfoReturnable<ActionResult> cir) {
         BlockPos hitBlockPos = hit.getBlockPos();
         BlockPos respawnAnchor = hitBlockPos.add(0,-1,0); // checks block beneath the egg
 
-
-        if (world.getBlockState(respawnAnchor) == Blocks.RESPAWN_ANCHOR.getDefaultState()) { //you know there's prob a better way but fuck you
-            System.out.println("PORN");
-
-            //ha fuck you
-            player.kill();
-            PlayerEntity
+        if (world.getBlockState(respawnAnchor).getBlock() instanceof RespawnAnchorBlock) {
+            System.out.println("has been click");
         }
-
-
-
     }
-
-
-
-
 }
