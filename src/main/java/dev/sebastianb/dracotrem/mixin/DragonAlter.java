4a52b6e577f110223f887c6b2f6b36c46e80f17a
SebaSphere <SebaSphereEmails@gmail.com>
2020-10-04 00:11:42 -0400

    Add Dragon Egg Shard recipe to make dragon egg.

--- src/main/java/dev/sebastianb/dracotrem/mixin/DragonAlter.java
+++ src/main/java/dev/sebastianb/dracotrem/mixin/DragonAlter.java
@@ -18,6 +18,7 @@ import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 
@@ -27,14 +28,22 @@ Wait it's all mixins!
 Always has been...
  */
 @Mixin(DragonEggBlock.class)
-public abstract class OnUse {
+public abstract class DragonAlter {
+
+    @Inject(method = "onBlockBreakStart", at = @At("HEAD"), cancellable = true)
+    private void preventBlockBreak(BlockState state, World world, BlockPos pos, PlayerEntity player, CallbackInfo ci) {
+        if (world.getBlockState(pos.add(0,-1,0)).getBlock() instanceof RespawnAnchorBlock) {
+            soundPlayer(player,world,pos,"eggError");
+            ci.cancel();
+        }
+    }
 
 
 
 
 
     @Inject(method = "onUse", at = @At("HEAD"), cancellable = true)
-    private void used(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit, CallbackInfoReturnable<ActionResult> cir) {
+    private void onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit, CallbackInfoReturnable<ActionResult> cir) {
         BlockPos hitBlockPos = hit.getBlockPos();
         BlockPos respawnAnchor = hitBlockPos.add(0,-1,0); // checks block beneath the egg
         boolean anchorUnderEgg = world.getBlockState(respawnAnchor).getBlock() instanceof RespawnAnchorBlock; //Boolean for if anchor is under a dragon egg
