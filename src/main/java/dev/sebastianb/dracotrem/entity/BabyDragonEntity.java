12b52e53671681697edaacd2a0dc519bb54f423e
Brendan Lewis <brendanelewis@outlook.com>
2020-10-15 17:20:20 -0400

    Rideable dragon

--- src/main/java/dev/sebastianb/dracotrem/entity/BabyDragonEntity.java
+++ src/main/java/dev/sebastianb/dracotrem/entity/BabyDragonEntity.java
@@ -1,5 +1,6 @@
 package dev.sebastianb.dracotrem.entity;
 
+import net.minecraft.block.BlockState;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.ai.RangedAttackMob;
@@ -8,12 +9,21 @@ import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.boss.BossBar;
 import net.minecraft.entity.boss.ServerBossBar;
 import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.mob.HostileEntity;
 import net.minecraft.entity.mob.MobEntity;
+import net.minecraft.entity.passive.HorseBaseEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.text.Text;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
@@ -25,7 +35,7 @@ import software.bernie.geckolib.event.AnimationTestEvent;
 import software.bernie.geckolib.manager.EntityAnimationManager;
 
 
-public class BabyDragonEntity extends HostileEntity implements RangedAttackMob, IAnimatedEntity {
+public class BabyDragonEntity extends HorseBaseEntity implements IAnimatedEntity {
 
     private final ServerBossBar bossBar;
     private final EntityAnimationManager manager;
@@ -33,9 +43,12 @@ public class BabyDragonEntity extends HostileEntity implements RangedAttackMob, 
 
     public BabyDragonEntity(EntityType<? extends BabyDragonEntity> entityType, World world) {
         super(entityType, world);
-        this.bossBar = (ServerBossBar)(new ServerBossBar(this.getDisplayName(), BossBar.Color.PURPLE, BossBar.Style.PROGRESS))
-                .setDarkenSky(true)
-                .setThickenFog(true);
+        this.bossBar = (ServerBossBar)(new ServerBossBar(
+                this.getDisplayName(),
+                BossBar.Color.PURPLE,
+                BossBar.Style.PROGRESS
+            )
+        ).setDarkenSky(false).setThickenFog(false);
         this.setHealth(this.getMaxHealth());
         this.experiencePoints = 50;
 
@@ -43,6 +56,100 @@ public class BabyDragonEntity extends HostileEntity implements RangedAttackMob, 
         this.controller = new EntityAnimationController(this, "flapController", 20, this::animationPredicate);
         this.manager.addAnimationController(controller);
     }
+
+    @Override
+    protected void initGoals() {}
+
+    @Override
+    public boolean isTame() {
+        return true;
+    }
+
+    @Override
+    public boolean eatsGrass() {
+        return false;
+    }
+
+    @Override
+    public boolean isSaddled() {
+        return true;
+    }
+
+    @Override
+    public void travel(Vec3d movementInput) {
+        if (this.isAlive()) {
+            if (this.hasPassengers() && this.canBeControlledByRider()) {
+                this.setVelocity(Vec3d.ZERO);
+                LivingEntity livingEntity = (LivingEntity)this.getPrimaryPassenger();
+                this.yaw = livingEntity.yaw;
+                this.prevYaw = this.yaw;
+                this.pitch = livingEntity.pitch * 0.5F;
+                this.setRotation(this.yaw, this.pitch);
+                this.bodyYaw = this.yaw;
+                this.headYaw = this.bodyYaw;
+                float f = livingEntity.sidewaysSpeed * 0.5F;
+                float g = livingEntity.forwardSpeed;
+                if (g <= 0.0F) {
+                    g *= 0.25F;
+                }
+                Vec3d vec3d = this.getVelocity();
+                this.setVelocity(vec3d.x, vec3d.y, vec3d.z);
+                this.setInAir(true);
+                this.velocityDirty = true;
+                if (g > 0.0F) {
+                    float i = MathHelper.sin(this.yaw * 0.017453292F);
+                    float j = MathHelper.cos(this.yaw * 0.017453292F);
+                    this.setVelocity(this.getVelocity().add((-0.4F * i), 0.0D, (0.4F * j)));
+                }
+                this.flyingSpeed = this.getMovementSpeed() * 0.1F;
+                if (this.isLogicalSideForUpdatingMovement()) {
+                    this.setMovementSpeed((float)this.getAttributeValue(EntityAttributes.GENERIC_MOVEMENT_SPEED));
+                    super.travel(new Vec3d(f, movementInput.y, g));
+                } else if (livingEntity instanceof PlayerEntity) {
+                    this.setVelocity(Vec3d.ZERO);
+                }
+                if (this.onGround) {
+                    this.setInAir(false);
+                }
+                this.method_29242(this, false);
+            } else {
+                this.flyingSpeed = 1.0F;
+                super.travel(movementInput);
+            }
+        }
+    }
+
+    @Override
+    public ActionResult interactMob(PlayerEntity player, Hand hand) {
+        ItemStack itemStack = player.getStackInHand(hand);
+
+        if (this.isTame() && player.shouldCancelInteraction()) {
+            this.openInventory(player);
+            return ActionResult.success(this.world.isClient);
+        }
+        if (this.hasPassengers()) {
+            return super.interactMob(player, hand);
+        }
+        if (!itemStack.isEmpty()) {
+            ActionResult actionResult = itemStack.useOnEntity(player, this, hand);
+            if (actionResult.isAccepted()) {
+                return actionResult;
+            }
+            boolean bl = itemStack.getItem() == Items.SADDLE;
+            if (this.isHorseArmor(itemStack) || bl) {
+                this.openInventory(player);
+                return ActionResult.success(this.world.isClient);
+            }
+        }
+        this.putPlayerOnBack(player);
+        return ActionResult.success(this.world.isClient);
+    }
+
+    @Override
+    protected void playWalkSound(BlockSoundGroup group) {}
+
+    @Override
+    protected void playStepSound(BlockPos pos, BlockState state) {}
 
     public static DefaultAttributeContainer.Builder createBabyDragonAttributes() {
         return MobEntity.createMobAttributes()
@@ -70,14 +177,6 @@ public class BabyDragonEntity extends HostileEntity implements RangedAttackMob, 
     protected void mobTick() {
         super.mobTick();
         this.bossBar.setPercent(this.getHealth() / this.getMaxHealth());
-
-        Vec3d vec3d = this.getVelocity();
-        Vec3d vec3d2 = vec3d.add((0.5D - vec3d.x) * 0.1D, (0.699999988079071D - vec3d.y) * 0.1D, (0.5D - vec3d.z) * 0.1D);
-        this.setVelocity(vec3d2);
-        float g = (float)(MathHelper.atan2(vec3d2.z, vec3d2.x) * 57.2957763671875D) - 90.0F;
-        float h = MathHelper.wrapDegrees(g - this.yaw);
-        this.forwardSpeed = 0.5F;
-        this.yaw += h;
     }
 
     @Override
@@ -116,10 +215,5 @@ public class BabyDragonEntity extends HostileEntity implements RangedAttackMob, 
     @Override
     protected float getSoundPitch() {
         return super.getSoundPitch() + 1.5F;
-    }
-
-    @Override
-    public void attack(LivingEntity target, float pullProgress) {
-        // TODO: attack
     }
 }
